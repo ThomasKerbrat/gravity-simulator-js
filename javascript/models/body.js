f2698fdab0d34f5d10ae080b0ddd92eb4ec975a2
Thomas Kerbrat <kerbrat@outlook.com>
2017-12-12 20:39:17 +0100

    Add semicolons

--- javascript/models/body.js
+++ javascript/models/body.js
@@ -1,31 +1,31 @@
 
 class Body {
     constructor(position, speed, acceleration, mass) {
-        this.position = position
-        this.speed = speed
-        this.acceleration = acceleration
-        this._mass = mass
-        this._radius = null
+        this.position = position;
+        this.speed = speed;
+        this.acceleration = acceleration;
+        this._mass = mass;
+        this._radius = null;
     }
 
     get radius() {
-        return this._radius === null ? this.computeRadius() : this._radius
+        return this._radius === null ? this.computeRadius() : this._radius;
     }
 
     get mass() {
-        return this._mass
+        return this._mass;
     }
 
     set mass(value) {
-        this._mass = value
-        this.computeRadius()
+        this._mass = value;
+        this.computeRadius();
     }
 
     computeRadius() {
         // Compute the radius for a sphere from volume = mass
-        return this._radius = Math.pow(3 / 4 * this._mass / Math.PI, 1 / 3) / 1e3
+        return this._radius = Math.pow(3 / 4 * this._mass / Math.PI, 1 / 3) / 1e4;
         // log10
-        // return this._radius = Math.log10(this._mass) / 2
-        // return this._radius = 2
+        // return this._radius = Math.log10(this._mass) / 2;
+        // return this._radius = 2;
     }
 }
--- javascript/models/vector.js
+++ javascript/models/vector.js
@@ -1,28 +1,28 @@
 
 class Vector {
     constructor(x, y) {
-        this.x = x
-        this.y = y
+        this.x = x;
+        this.y = y;
     }
 
     angle() {
-        return Math.atan2(this.y, this.x)
+        return Math.atan2(this.y, this.x);
     }
 
     add(vector) {
-        this.x += vector.x
-        this.y += vector.y
+        this.x += vector.x;
+        this.y += vector.y;
     }
 
     static angle(vector1, vector2) {
         if (arguments.length === 1)
-            return Math.atan2(vector1.y, vector1.x)
+            return Math.atan2(vector1.y, vector1.x);
         else if (arguments.length === 2) {
-            return Math.atan2(vector2.y - vector1.y, vector2.x - vector1.x)
+            return Math.atan2(vector2.y - vector1.y, vector2.x - vector1.x);
         }
     }
 
     static null() {
-        return new Vector(0, 0)
+        return new Vector(0, 0);
     }
 }
